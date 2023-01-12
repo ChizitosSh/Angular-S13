71e2f2293ce45cac2358a59d32adcc705473f63e
MariCarolV9 <118744765+MariCarolV9@users.noreply.github.com>
2023-01-12 10:16:57 -0500

    Update data.component.ts

--- projects/s13-sincrona/src/app/pipes/data/data.component.ts
+++ projects/s13-sincrona/src/app/pipes/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
+  
+  //
+  constructor(private formBuilder: FormBuilder){}
+
+  formPipes = this.formBuilder.group({
+    valor: ['', {validators: [Validators.pattern('^[A-Z a-z]+$')]}],
+  });
+
+  get valor(){ return this.formPipes.get('valor'); }
+  
+  //
+  
   title: string = 'uso de pipes';
   upperCase!: string;
   currency!: number;
